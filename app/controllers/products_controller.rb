2f3870ae21ef0cad9d2e92d74f3483984aea7c4c
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-08 15:42:54 -0300

    fix name of field

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name, :unit_price)
     end
 end

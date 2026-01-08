80227d219a090d2c893f401012f22172dd7a544d
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-08 15:45:08 -0300

    update routes table

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
-  resources :carts, only: %i[index]
-
-  get 'cart', to: 'carts#index'
+  resources :carts, only: %i[index] do
+    resources :card_products, only: %i[create]
+  end
 
   resources :products
+
+  get 'cart', to: 'carts#index'
   get "up" => "rails/health#show", as: :rails_health_check
+
+  post 'cart', to: 'card_products#create'
 
   root "rails/health#show"
 end

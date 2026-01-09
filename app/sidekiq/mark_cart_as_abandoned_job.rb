292da5643af0d4823ddcff4b794d14a2d337c458
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-08 23:38:24 -0300

    update job MarkCartAsAbandonedJob to set abandoned status of Cart

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,14 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  def perform
+    now = Time.zone.now
+    delta_time_to_abandon = ENV.fetch("TIME_TO_MARK_AS_ABANDONED_IN_SECONDS", 3 * 60 * 60).to_i.seconds
+
+    Cart.where(status: 'ACTIVE').find_each do |cart|
+      next if cart.last_interaction_at.present? && (now - cart.last_interaction_at) <= delta_time_to_abandon
+
+      cart.update(status: 'ABANDONED')
+    end
   end
 end

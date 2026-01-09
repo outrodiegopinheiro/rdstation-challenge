92dbcf268e303515825c82a9aeaf34db3f45c50b
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-09 04:27:07 -0300

    add tests to MarkCartAsAbandonedJob

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -3,19 +3,11 @@ class MarkCartAsAbandonedJob
 
   def perform
     now = Time.zone.now
-    delta_time_to_abandon = ENV.fetch("TIME_TO_MARK_AS_ABANDONED_IN_SECONDS", 3 * 60 * 60).to_i.seconds
 
-    Cart.where(status: 'ACTIVE').find_each do |cart|
-      next if cart.last_interaction_at.present? && (now - cart.last_interaction_at) <= delta_time_to_abandon
-
-      cart.update(status: 'ABANDONED', marked_as_abandoned_at: now)
-    end
+    Cart.where(status: 'ACTIVE').each { |cart| cart.mark_as_abandoned(datetime: now) }
 
     ###
 
-    delta_days_to_destroy = ENV.fetch("TIME_TO_DESTROY_ABANDONED_CART_IN_DAYS", 7).to_i.days
-    datetime_to_test_to_destroy = now - delta_days_to_destroy
-
-    Cart.where(status: 'ABANDONED', marked_as_abandoned_at: ...datetime_to_test_to_destroy).each(&:destroy)
+    Cart.where(status: 'ABANDONED').each { |cart| cart.remove_if_abandoned(datetime: now) }
   end
 end
--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,18 @@
 require 'rails_helper'
+require 'sidekiq/testing'
+
+Sidekiq::Testing.fake! # fake is the default mode
+
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+  describe "#perform_later" do
+    it "run" do
+      expect {
+        MarkCartAsAbandonedJob.perform_async
+      }.to change(MarkCartAsAbandonedJob.jobs, :size).by(1)
+
+      MarkCartAsAbandonedJob.drain
+
+      expect(MarkCartAsAbandonedJob.jobs.size).to eq(0)
+    end
+  end
 end

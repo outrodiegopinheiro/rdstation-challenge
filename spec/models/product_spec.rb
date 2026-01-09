277486d0b954efed180826ebf22df832be7091fb
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-09 01:06:09 -0300

    fix tests for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,21 +3,24 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   context 'when validating' do
     it 'validates presence of name' do
-      product = described_class.new(price: 100)
+      product = described_class.new(unit_price: 100)
+
       expect(product.valid?).to be_falsey
       expect(product.errors[:name]).to include("can't be blank")
     end
 
-    it 'validates presence of price' do
+    it 'validates presence of unit_price' do
       product = described_class.new(name: 'name')
+
       expect(product.valid?).to be_falsey
-      expect(product.errors[:price]).to include("can't be blank")
+      expect(product.errors[:unit_price]).to include("can't be blank")
     end
 
-    it 'validates numericality of price' do
-      product = described_class.new(price: -1)
+    it 'validates numericality of unit_price' do
+      product = described_class.new(unit_price: -1)
+
       expect(product.valid?).to be_falsey
-      expect(product.errors[:price]).to include("must be greater than or equal to 0")
+      expect(product.errors[:unit_price]).to include("must be greater than or equal to 0")
     end
   end
 end

78a540260f6874076f326b472e7e466409e1ad1f
Diego Pinheiro <outrodiegopinheiro@gmail.com>
2026-01-09 03:08:36 -0300

    tests for requests in /carts

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "/carts", type: :request do
-  pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
-  describe "POST /add_items" do
-    let(:cart) { Cart.create }
-    let(:product) { Product.create(name: "Test Product", price: 10.0) }
-    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
+  let(:valid_headers) {
+    {}
+  }
 
-    context 'when the product already is in the cart' do
-      subject do
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-      end
+  describe "GET /index" do
+    it "renders a successful response" do
+      get cart_url(1), headers: valid_headers, as: :json
 
-      it 'updates the quantity of the existing item in the cart' do
-        expect { subject }.to change { cart_item.reload.quantity }.by(2)
-      end
+      expect(response).to be_successful
     end
   end
 end
